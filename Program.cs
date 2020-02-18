a531de7951a66c9c9c8c6903198acaaa9030e268
Daniel <danielborg291@gmail.com>
2020-02-18 12:19:31 -0800

    Perception Array Mapping
    removed unused code and formated code

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 ﻿using CognitiveABM.FCM;
+using PerceptronMaker;
+using System;
 
 namespace GeneticAlgorithm
 {
-    class Program
-    {
-        public static void Main()
-        {
-           FCMSplittingAlgorithm fcm = new FCMSplittingAlgorithm(5000,423,10000);
-            fcm.Run();
-        }
-    }
+	class Program
+	{
+		public static void Main()
+		{
+			Perceptron perceptron = new Perceptron(9, 2, 9, 9);
+			Console.WriteLine(perceptron.ToString());
+			FCMSplittingAlgorithm fcm = new FCMSplittingAlgorithm(5000, 423, 10000);
+			fcm.Run();
+		}
+	}
 }
