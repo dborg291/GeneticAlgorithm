a4bc239caa2c943448a47889c7f65ef2f566db2b
alexweininger <alex.weininger@live.com>
2020-03-22 00:51:19 -0700

    Perceptron calculation

--- Program.cs
+++ Program.cs
@@ -8,10 +8,13 @@ namespace CognitiveABM
 	{
 		public static void Main()
 		{
-			PerceptronFactory perceptron = new PerceptronFactory(9, 2, 9, 9);
-			Console.WriteLine(perceptron.ToString());
-			HillClimberFCM fcm = new HillClimberFCM(1000, 423, 5000);
-			fcm.Run();
+			PerceptronFactory perceptron = new PerceptronFactory(2, 2, 2, 1);
+
+            perceptron.CalculatePerceptron(new double[]{0.3, 0.5, 1, 0, 0.2, 0.1, 0.7, 0.3, 0.5, 1, 0, 0.2, 0.1, 0.7, 0.3, 0.5, 1, 0, 0.2, 0.1, 0.7, 0.3, 0.5, 1, 0, 0.2, 0.1, 0.7, 0.3, 0.5, 1, 0, 0.2, 0.1, 0.7, 0.3, 0.5, 1, 0, 0.2, 0.1, 0.7}, new double[]{1, 0.5});
+
+			// Console.WriteLine(perceptron.ToString());
+			// HillClimberFCM fcm = new HillClimberFCM(1000, 423, 5000);
+			// fcm.Run();
 		}
 	}
 }
